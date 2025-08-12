4ed539f67109600b39f71472a95c0c544a6b7377
Abrar Shahrier Arnab <120324766+arnob56@users.noreply.github.com>
2025-08-12 20:47:22 +0600

    first commit

---
+++ try001.js
@@ -0,0 +1,24 @@
+const profile={
+    name: "Shahrier Arnob" ,
+    followers : 374,
+    following : 1000,
+    post: [
+        {
+            id: 1,
+            title: "My First Post",
+            content: "This is the content of my first post. I am excited to share my journey with you all!",
+            date: "2023-10-01"
+        },
+        {
+            id: 2,
+            title: "Learning JavaScript",
+            content: "JavaScript is an amazing language for web development. I love how versatile it is!",
+            date: "2023-10-05"
+        }
+    ],
+    bio: "Thank you for visiting my profile. I am a web developer with a passion for creating interactive and dynamic web applications.",
+    location: "Dhaka, Bangladesh",
+    website: "https://shahrierarnob.com",
+}
+
+console.log(profile);
