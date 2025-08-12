f8af36dd9b90b8e9327ca63181198d035105cc7f
arnob550 <abrarshahrierarnab@gmail.com>
2025-08-12 22:06:14 +0600

    Multiple commimt

---
+++ practice002.js
@@ -0,0 +1,9 @@
+//multiple of 7 checker
+
+let num =prompt("Enter a number:");
+if (num % 7 === 0) {
+    console.log("The number is divisible by 7");
+} 
+else {
+    console.log("The number is not divisible by 7");
+}
