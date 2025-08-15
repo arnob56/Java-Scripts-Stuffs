666a0c031612cf7e5434964def8fe7071de44b79
arnob550 <abrarshahrierarnab@gmail.com>
2025-08-15 23:59:03 +0600

    day4

--- try004.js
+++ try004.js
@@ -1,1 +1,12 @@
- 
+ //Arrays
+
+ let arr = [120, 150, 180, 200, 220, 250, 270, 310, 350, 400];
+ const arr2 = [100, 200, 300, 400, 500];
+ console.log(arr[0]); // 120
+ console.log(arr.length);
+
+ //looping through arrays
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
