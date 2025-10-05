6a7abd93ea4d3b98aa5c5681bd6e6d38d718c6ae
Abrar Shahrier Arnab <120324766+arnob56@users.noreply.github.com>
2025-10-05 14:06:28 +0600

    Update practice005.js

--- practice005.js
+++ practice005.js
@@ -1,2 +1,12 @@
+//function create
 function vowelFinder(string){
-  
+  let count=0;
+  for (const char of string){
+    if(char==="a" || char==="e" || char==="i" || char==="o" || char==="u"){
+      count++;
+    }
+  }
+
+  //function call
+  function vowelFinder(GitHub);
+      
